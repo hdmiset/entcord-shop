49603f7d0e8f1b94c14cef7fa2edda81d7a4e52f
Vadim Petrusenko <v.v.petrusenko@mail.ru>
2019-03-16 20:47:16 +0300

    Completed main page + footer + navbar

--- webapp/src/main/java/com/petrusenko/webapp/controller/PageController.java
+++ webapp/src/main/java/com/petrusenko/webapp/controller/PageController.java
@@ -4,19 +4,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 public class PageController {
-	
-	@RequestMapping(value = {"/", "/home", "/index"})
-	
+
+	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
-		
+
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to spring web mvc");
-		
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
+
 		return mv;
-		
+
+	}
+
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About us");
+		mv.addObject("userClickAbout", true);
+
+		return mv;
+
+	}
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact us");
+		mv.addObject("userClickContact", true);
+
+		return mv;
+
 	}
 
 }
---
+++ webapp/src/main/webapp/WEB-INF/views/shared/footer.jsp
@@ -0,0 +1,14 @@
+    <div class="container footer">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; 2019 EntcordShop. All rights reserved</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
